a1e085e37874bfa2ce5f983d8ca7e7ff5033dc8e
andmironov <thatsmironov@gmail.com>
2015-10-19 23:06:22 +0300

    prototype

--- app/main.js
+++ app/main.js
@@ -1,46 +1,67 @@
 let React = require("react");
+var ReactDOM = require('react-dom');
 let Item = require("./item.js");
-var ItemsStore = require('stores/ItemsStore.js');
-
-let request = require("superagent");
 let styles = require("./less/main.less");
 
 function getItemsState() {
-  return {
-    allItems: ItemsStore.getAll()
-  }
+  return {}
 }
 
 var RemoteJobsApp = React.createClass({
 
   getInitialState: function() {
-    return getTodoState();
+    return getItemsState();
   },
 
   componentDidMount: function() {
-    ItemsStore.addChangeListener(this._onChange);
+    document.querySelectorAll(".phoneButton")[0].addEventListener("click", function(argument) {
+      document.querySelectorAll(".phoneBG")[0].classList.add("active");
+    });
   },
 
   componentWillUnmount: function() {
-    ItemsStore.removeChangeListener(this._onChange);
+
   },
 
   render: function() {
+
+    function buttonClick() {
+      document.querySelectorAll(".phoneButton")[0].classList.add("onClick");
+      setTimeout(startLoading, 200)
+
+    }
+
+    function startLoading() {
+      document.querySelectorAll(".phoneBG")[0].classList.add("active");
+      document.querySelectorAll(".phoneButton")[0].classList.remove("onClick");
+    }
+
+    function loadingComplete() {
+      document.querySelectorAll(".phoneBG")[0].classList.add("complete");
+    }
+
+    function startOver() {
+      document.querySelectorAll(".phoneBG")[0].classList.remove("active");
+      document.querySelectorAll(".phoneBG")[0].classList.remove("complete");
+    }
+
     return (
       <div>
-        <Header />
-        <MainSection
-          allTodos={this.state.allTodos}
-          areAllComplete={this.state.areAllComplete}
-        />
-        <Footer allTodos={this.state.allTodos} />
+        <Item/>
+        <button onClick={buttonClick}>buttonClick</button><br/>
+        <button onClick={loadingComplete}>loadingComplete</button><br/>
+        <button onClick={startOver}>startOver</button>
       </div>
-    );
+
+    )
   },
 
   _onChange: function() {
-    this.setState(getTodoState());
+    this.setState(getItemsState());
   }
-});
 
+
+
+});
+React.render(<RemoteJobsApp name="John" />, document.querySelectorAll(".app")[0]);
 module.exports = RemoteJobsApp;
